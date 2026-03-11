1ae82a850448e3c519a83ef1b099ccfd5638d30f
wongshiyuen <67384218+wongshiyuen@users.noreply.github.com>
2026-03-11 22:31:20 +0800

    Add files via upload

--- marketQuery.sql
+++ marketQuery.sql
@@ -24,7 +24,7 @@ SELECT * FROM supermarket_sales LIMIT 5;
 \copy (SELECT branch, SUM(unit_price*quantity) AS total_sales, SUM(tax_5percent) AS total_taxes FROM supermarket_sales GROUP BY branch ORDER BY total_sales DESC) TO 'branch_sales.csv' WITH CSV HEADER;
 
 -- QUERY 2: IDENTIFY PRODUCT LINE WITH HIGHEST SALES PER BRANCH
-\copy (SELECT branch, product_line, total_sales, total_taxes FROM (SELECT branch, product_line, SUM(unit_price*quantity) AS total_sales, SUM(tax_5percent) AS total_taxes, RANK() OVER (PARTITION BY branch ORDER BY SUM(unit_price*quantity) DESC) FROM supermarket_sales GROUP BY branch, product_line)) TO 'branch_bestsellers.csv' WITH CSV HEADER;
+\copy (SELECT branch, product_line, SUM(unit_price * quantity) AS total_sales, SUM(tax_5percent) AS total_taxes FROM supermarket_sales GROUP BY branch, product_line ORDER BY total_sales DESC) TO 'branch_product_sales.csv' WITH CSV HEADER;
 
 --'\copy' is used to convert results in CSV format.
 --NOTE: '\copy' is PSQL command code; must be written on a single line.
